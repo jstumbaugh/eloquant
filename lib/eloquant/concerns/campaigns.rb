a915356217904704da9cfc584279c65bb13d628a
Jason Stumbaugh <stumbaughjason@gmail.com>
2018-10-30 17:09:25 -0500

    Add campaign endpoints

--- lib/eloquant/concerns/campaigns.rb
+++ lib/eloquant/concerns/campaigns.rb
@@ -8,6 +8,19 @@ module Eloquant
       describe_endpoint_csv(campaigns)
     end
 
+    def number_of_campaigns
+      get_campaigns(count: 1).try(:[], :total)
+    end
+
+    def get_campaigns(page: nil, depth: :minimal, count: nil)
+      params = {}
+      params[:page]  = page  if !page.nil?
+      params[:depth] = depth if !depth.nil?
+      params[:count] = count if !count.nil?
+
+      get("/api/rest/2.0/assets/campaigns", params)
+    end
+
     private
 
       def campaigns
